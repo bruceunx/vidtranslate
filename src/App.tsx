46e4c5a06c32054aa50460b329eef8e7d2aaaf26
bruce <cnhxunoo@gmail.com>
2024-04-06 16:42:45 +0800

    add selection in footer in left menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,87 @@
-import { useState, useEffect } from 'react';
-import { invoke } from '@tauri-apps/api/tauri';
-import { listen } from '@tauri-apps/api/event';
-import './App.css';
+// import { useEffect } from 'react';
+// import { invoke } from '@tauri-apps/api/tauri';
+// import { listen } from '@tauri-apps/api/event';
+// import { open } from '@tauri-apps/api/dialog';
+// import { platform } from '@tauri-apps/api/os';
 
-type Message = {
-  event: string;
-  payload: {
-    message: string;
-  };
-};
+import { AiOutlinePlus, AiOutlineSetting } from 'react-icons/ai';
+import { HiEllipsisHorizontal } from 'react-icons/hi2';
+
+import './styles.css';
+import LangDetect from './components/LangDetect';
+
+// type Message = {
+//   event: string;
+//   payload: {
+//     message: string;
+//   };
+// };
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState('');
-  const [name, setName] = useState('');
-  const [enable, setEnable] = useState(true);
+  // async function greet() {
+  //   await invoke('async_stream', { name_str: name });
+  //   const selected = await open({
+  //     multiple: true,
+  //     filters: [
+  //       {
+  //         name: 'Image',
+  //         extensions: ['png', 'jpeg'],
+  //       },
+  //     ],
+  //   });
+  //   console.log(selected);
+  // }
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    await invoke('async_stream', { name_str: name });
-  }
-  useEffect(() => {
-    listen('greet', (e: Message) => {
-      const message = e.payload.message;
-      if (message === 'stop') {
-        setEnable(true);
-      } else {
-        setEnable(false);
-        setGreetMsg(message);
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const platformInfo = await platform();
+  //     console.log(platformInfo);
+  //   })();
+  //
+  //   listen('greet', (e: Message) => {
+  //     const message = e.payload.message;
+  //     if (message === 'stop') {
+  //       setEnable(true);
+  //     } else {
+  //       setEnable(false);
+  //       setGreetMsg(message);
+  //     }
+  //   });
+  // }, []);
 
   return (
-    <div className="container">
-      <h1 data-tauri-drag-region>Welcome to new world!</h1>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        {enable && <button type="submit">Greet</button>}
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
+    <>
+      <div className="flex w-full h-screen">
+        <div className="flex flex-col w-64 h-full bg-gray-950/90 text-white justify-stretch">
+          <div className="w-full bg-black/95 text-gray-300">
+            <div
+              data-tauri-drag-region
+              className="flex flex-row justify-end content-end py-2 pr-1 space-x-2"
+            >
+              <button>
+                <AiOutlinePlus />
+              </button>
+              <button>
+                <AiOutlineSetting />
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col px-2 py-1 h-full">
+            <p>list</p>
+          </div>
+          <div className="flex w-full p-2 border-t border-gray-700 justify-between text-sm px-2">
+            <LangDetect />
+            <button>
+              <HiEllipsisHorizontal />
+            </button>
+          </div>
+        </div>
+        <div className="w-full h-full bg-green-200">
+          <p>main window</p>
+          <p>direction window</p>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/LangDetect.tsx
@@ -0,0 +1,35 @@
+import { HiOutlineChevronUpDown } from 'react-icons/hi2';
+import * as Select from '@radix-ui/react-select';
+
+const LangDetect = () => {
+  return (
+    <Select.Root>
+      <Select.Trigger
+        className="flex flex-row w-40 justify-between border border-gray-600 rounded-md py-1 px-2 hover:text-gray-300"
+        aria-label="lang"
+      >
+        <Select.Value placeholder="Auto detection" />
+        <Select.Icon className="pt-1">
+          <HiOutlineChevronUpDown />
+        </Select.Icon>
+      </Select.Trigger>
+      <Select.Portal>
+        <Select.Content className="text-gray-300 z-10 bg-gray-700 p-1 rounded-md">
+          <Select.Viewport>
+            <Select.Item value="auto" className="hover:bg-gray-900">
+              <Select.ItemText>Auto detection</Select.ItemText>
+            </Select.Item>
+            <Select.Item value="english" className="hover:bg-gray-900">
+              <Select.ItemText>English</Select.ItemText>
+            </Select.Item>
+            <Select.Item value="chinese" className="hover:bg-gray-900">
+              <Select.ItemText>中文</Select.ItemText>
+            </Select.Item>
+          </Select.Viewport>
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
+  );
+};
+
+export default LangDetect;
