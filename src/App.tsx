3fd8074b090865746efcfe2d15d43d24244284de
bruce <cnhxunoo@gmail.com>
2024-04-09 07:28:43 +0800

    add right siderbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 // import { useEffect } from 'react';
 // import { invoke } from '@tauri-apps/api/tauri';
 // import { listen } from '@tauri-apps/api/event';
@@ -8,7 +9,9 @@ import { AiOutlinePlus, AiOutlineSetting } from 'react-icons/ai';
 import {
   HiEllipsisHorizontal,
   HiMiniArrowLeftOnRectangle,
+  HiMiniArrowRightOnRectangle,
   HiMiniPlay,
+  HiMiniPause,
 } from 'react-icons/hi2';
 
 import './styles.css';
@@ -23,6 +26,16 @@ import NSlider from './components/Slider';
 // };
 
 function App() {
+  const [isPlay, setIsPlay] = React.useState<boolean>(true);
+  const [showRightSider, setShowRightSider] = React.useState<boolean>(true);
+
+  const togglePlay = () => {
+    setIsPlay((prev) => !prev);
+  };
+
+  const toggleRightSider = () => {
+    setShowRightSider((prev) => !prev);
+  };
   // async function greet() {
   //   await invoke('async_stream', { name_str: name });
   //   const selected = await open({
@@ -72,6 +85,7 @@ function App() {
             </div>
           </div>
           <div className="flex flex-col px-2 py-1 h-full">
+            {/* Scroll Area Radix-UI */}
             <p>list</p>
           </div>
           <div className="flex w-full p-3 border-t border-gray-700 justify-between text-sm px-2">
@@ -88,28 +102,43 @@ function App() {
           >
             <p className="">title</p>
             <div className="space-x-2">
-              <button>
-                <HiMiniArrowLeftOnRectangle />
+              <button onClick={toggleRightSider}>
+                {showRightSider ? (
+                  <HiMiniArrowRightOnRectangle />
+                ) : (
+                  <HiMiniArrowLeftOnRectangle />
+                )}
               </button>
               <button>
                 <HiEllipsisHorizontal />
               </button>
             </div>
           </div>
-          <div className="flex flex-col justify-stretch bg-gray-950/95 text-white h-full px-3">
-            <div className="h-full">
-              <p>main window</p>
-              <p>direction window</p>
-            </div>
-            <div className="flex flex-col items-center justify-center h-20">
-              <NSlider />
-              <div className="flex justify-between w-full h-fit">
-                <button className="hover:text-gray-400">
-                  <HiMiniPlay className="h-7 w-7" />
-                </button>
-                <p className="text-sm">00.00/12.22</p>
+          <div className="flex flex-row justify-stretch bg-gray-950/95 text-white h-full pl-3">
+            <div className="flex flex-col w-full justify-stretch h-full pr-3">
+              <div className="h-full">
+                <p>main window</p>
+                <p>direction window</p>
+              </div>
+              <div className="flex flex-col items-center justify-center h-20">
+                <NSlider />
+                <div className="flex justify-between w-full h-fit">
+                  <button className="hover:text-gray-400" onClick={togglePlay}>
+                    {isPlay ? (
+                      <HiMiniPause className="h-7 w-7" />
+                    ) : (
+                      <HiMiniPlay className="h-7 w-7" />
+                    )}
+                  </button>
+                  <p className="text-sm">00:00/12:22</p>
+                </div>
               </div>
             </div>
+            {showRightSider && (
+              <div className="flex flex-col w-full bg-sky-500">
+                <p>drawing</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -3,15 +3,15 @@ import * as Slider from '@radix-ui/react-slider';
 const NSlider = () => {
   return (
     <Slider.Root
-      className="relative flex items-center h-5 w-full select-none touch-none"
+      className="relative flex items-center h-5 w-full select-none touch-none hover:cursor-pointer"
       defaultValue={[50]}
       max={100}
-      step={1}
+      step={0.1}
     >
       <Slider.Track className="relative grow h-1 bg-gray-700 rounded-full">
         <Slider.Range className="absolute bg-violet-600/70 h-full round-full" />
       </Slider.Track>
-      <Slider.Thumb className="block w-3 h-3 bg-gray-400 rounded-full hover:bg-gray-300 focus:outline-none" />
+      <Slider.Thumb className="block w-3 h-3 bg-gray-400 rounded-full hover:bg-gray-300 hover:cursor-pointer focus:outline-none" />
     </Slider.Root>
   );
 };
